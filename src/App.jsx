2b4941d1e6c3a4034b6e52638ce6293b5fe91e0c
Vasta_git <askatseitakunov@gmail.com>
2024-05-08 23:07:24 +0300

    setting up tailwindcss and Login component with isLoggedIn logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import HomePage from './components/HomePage.jsx';
@@ -6,26 +6,59 @@ import CourseSchedule from './components/CourseSchedule.jsx';
 import AcademicRecord from './components/AcademicRecord.jsx';
 import Registration from './components/Registration.jsx';
 import studentInfo from './data/studentInfo.json';
+import Login from './components/Login.jsx';
 import './styles/styles.css';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userRole, setUserRole] = useState('');
+
+  const handleLogin = (username, password) => {
+    setIsLoggedIn(true);
+    console.log(username, password);
+    // setUserRole(role);
+  };
+
   return (
     <Router>
       <div>
-        <Navbar />
+        {isLoggedIn && <Navbar />} {/* Navbar is only rendered if isLoggedIn is true */}
         <main className="container">
           <Switch>
+            <Route exact path="/">
+              {isLoggedIn ? (
+                <Redirect to="/home" />
+              ) : (
+                <Login onLogin={handleLogin} />
+              )}
+            </Route>
             <Route path="/home">
-              <HomePage courses={studentInfo.courses} gpa={studentInfo.gpa} />
+              {isLoggedIn ? (
+                <HomePage courses={studentInfo.courses} gpa={studentInfo.gpa} />
+              ) : (
+                <Redirect to="/" />
+              )}
             </Route>
             <Route path="/courseschedule">
-              <CourseSchedule schedule={studentInfo.schedule} />
+              {isLoggedIn ? (
+                <CourseSchedule schedule={studentInfo.schedule} />
+              ) : (
+                <Redirect to="/" />
+              )}
             </Route>
             <Route path="/academicrecord">
-              <AcademicRecord academicRecord={studentInfo.academicRecord} />
+              {isLoggedIn ? (
+                <AcademicRecord academicRecord={studentInfo.academicRecord} />
+              ) : (
+                <Redirect to="/" />
+              )}
             </Route>
             <Route path="/registration">
-              <Registration />
+              {isLoggedIn ? (
+                <Registration />
+              ) : (
+                <Redirect to="/" />
+              )}
             </Route>
             <Route path="*">
               <Redirect to="/home" />
---
+++ src/components/Login.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+// import { useNavigate } from 'react-router-dom';
+
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [usernameError, setUsernameError] = useState('')
+  const [passwordError, setPasswordError] = useState('')
+
+  const handleLogin = () => {
+    if (!username) {
+      setUsernameError('Username cannot be empty')
+    } else {
+      setUsernameError('')
+    }
+
+    if (!password) {
+      setPasswordError('Password cannot be empty')
+    } else {
+      setPasswordError('')
+    }
+
+    if (username && password) {
+      onLogin(username, password)
+    }
+  }
+
+  return (
+    <div className='flex items-center justify-center mt-48'>
+      <div
+        className='rounded-lg border-4 border-black w-2/6'
+        style={{ boxShadow: '6px 6px 0px 0px rgba(0, 0, 0, 1)' }}
+      >
+
+        <div className='p-8'>
+        <h2 className='bg-[#A41F13] text-2xl font-bold text-black text-center mb-6 -mx-8 -mt-8'>
+          Login
+        </h2>
+          <div className='mb-4'>
+            <label
+              className='block text-black text-m font-bold mb-2'
+              htmlFor='username'
+            >
+              Username
+            </label>
+            <input
+              className='w-full border-2 border-black rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500'
+              type='text'
+              id='username'
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
+            <p style={{ color: 'red', fontWeight: 'bold' }}>{usernameError}</p>
+          </div>
+          <div className='mb-4'>
+            <label
+              className='block text-black text-m font-bold mb-2'
+              htmlFor='password'
+            >
+              Password
+            </label>
+            <input
+              className='w-full border-2 border-black rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500'
+              type='password'
+              id='password'
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <p style={{ color: 'red', fontWeight: 'bold' }}>{passwordError}</p>
+          </div>
+          <button
+            className='bg-white text-lg font-bold text-black py-2 px-4 rounded-lg border-4 border-black  transition duration-300 hover:scale-105'
+            onClick={handleLogin}
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
