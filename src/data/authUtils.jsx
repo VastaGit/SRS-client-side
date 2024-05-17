9511ba412112382b4ea5c6c2bff5554b76d7ce7a
Bilo-rm <bilal2002mahfouth@gmail.com>
2024-05-17 08:50:24 +0200

    .

--- src/data/authUtils.jsx
+++ src/data/authUtils.jsx
@@ -3,7 +3,7 @@ const simulateUserAuthentication = () => {
     // Mock user data
     const mockUserData = {
       username: 'john_doe',
-      role: 'admin', // Change role as needed for testing
+      role: 'advisor', // Change role as needed for testing
     };
   
     // Store user information in local storage
